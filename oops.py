17957eebe1e5f6c107dd42cf779905f382fbc556
dhinakaran-08 <dhinakaranvnr2020@gmail.com>
2025-10-07 11:35:45 +0530

    inheritence added

--- oops.py
+++ oops.py
@@ -12,3 +12,26 @@ obj = Child()
 obj.age()
 obj.hair()
 obj.eye()
+
+'''Inheritance'''
+
+class Animal:
+    def __init__(self,name,eye):
+        self.name=name
+        self.eye=eye
+    def info(self):
+        return f"{self.name} has four legs and {self.eye} coloured eyes"
+class Dog(Animal):
+    def __init__(self,name,eye):
+        super().__init__(name,eye)
+    def bark(self):
+        return f"{self.info()} and also {self.name} is barking"
+class Cat(Animal):
+    def __init__(self,name,eye):
+        super().__init__(name,eye)
+    def meow(self):
+        return f"{super().info()} and also {self.name} is meow"
+dog = Dog("rocky","brown")
+cat = Cat("poonachiii","blue")
+print(dog.bark())
+print(cat.meow())
